47ef87f171778056abad3341d83fd98c1040f8f5
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-17 23:56:58 +0300

    add  RouterView

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <script setup lang="ts">
 
+
 </script>
 
 <template>
 
+
+    <RouterView #default="{ Component, route}">
+      <template v-if="Component">
+        <component :is="Component" :key="route.fullPath"/>
+
+      </template>
+
+    </RouterView>
+
+
 </template>
 
 <style scoped>
+
 
 </style>
