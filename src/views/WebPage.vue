67558fc24dde172eaf462cf3fef3e3fe1ddaf172
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-07-17 23:50:55 +0300

    add WebPage component

---
+++ src/views/WebPage.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+import NavBar from '@/components/NavBar.vue''
+</script>
+
+<template>
+  <div class="h-screen ">
+    <NavBar />
+    <RouterView #default="{ Component, route}">
+      <template v-if="Component">
+        <component :is="Component" :key="route.fullPath"/>
+
+      </template>
+
+    </RouterView>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
