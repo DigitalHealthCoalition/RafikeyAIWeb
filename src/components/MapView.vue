38e343519eec2791916e4c3fb28d1581a773bf61
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-28 10:23:56 +0300

    add MapView component

---
+++ src/components/MapView.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+
+import { onMounted } from 'vue'
+import * as L from 'leaflet'
+
+onMounted(() => {
+  const map = L.map('map').setView([-1.286389, 36.817223], 7) // Nairobi coords
+
+  // Add the title layer from OpenStreetMap
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: ''
+  }).addTo(map)
+
+  // Add marker and popup
+  L.marker([-1.286389, 36.817223])
+    .addTo(map)
+    .bindPopup('Wellness Hub Clinic<br>123 Health Ave, Nairobi')
+    .openPopup()
+
+  // console.log('Map pane', map.getPane)
+})
+</script>
+
+<template>
+  <div
+    id="map"
+    class="h-[70vh] rounded-lg shadow-md flex items-center justify-center"
+  ></div>
+
+</template>
+
+<style scoped>
+#map {
+  z-index: 0 !important;
+}
+</style>
