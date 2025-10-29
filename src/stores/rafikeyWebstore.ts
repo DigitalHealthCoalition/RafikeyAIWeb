4c93b95aa82de1fabd7912eea0ee9151f05e888e
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-10-29 09:15:47 +0300

    add getClinics function

--- src/stores/rafikeyWebstore.ts
+++ src/stores/rafikeyWebstore.ts
@@ -6,7 +6,39 @@ import { useStorage } from '@vueuse/core'
 export const useRafikeyWebstore = defineStore('rafikeyWebstore', () => {
 
 const collapseSidebarSmall = ref(true);
+const BASE_URL = import.meta.env.VITE_BACKEND_URL_API
+
   const openChatFrame = useStorage('isChatOpen', false)
+  async function getClinics () {
+    // fetch clinics from api
+ try{
+   const response = await fetch(`${BASE_URL}/clinics/clinics`, {
+       method: 'GET',
+       headers: {
+         'Content-Type': 'application/json'
+       }
+     }
+   )
+   const data  = await response.json()
+   // console.log(data)
+   if(!response.ok) {
+     return {
+       result: 'error',
+       data: data.message
+     }
+   } else {
+     return {
+        result: 'success',
+        data: data
+     }
+   }
+ } catch(err){
+   console.log(err)
+
+
+ }
+
+  }
 
   const setCollapseSidebarSmall = (val: boolean) =>{
     collapseSidebarSmall.value = val
@@ -20,7 +52,8 @@ const collapseSidebarSmall = ref(true);
     collapseSidebarSmall,
     setCollapseSidebarSmall,
     openChatFrame,
-    setOpenChatFrame
+    setOpenChatFrame,
+    getClinics
 
   }
 
