bd00e84744a7b3566f0efcaa166b1fd6f224976a
dev-lyn <linahmuhonjaimbari@gmail.com>
2025-11-03 15:05:31 +0300

    add LoadingShimmer component

---
+++ src/components/LoadingShimmer.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    <div class="space-y-2">
+      <div class="h-6 animate-shimmer bg-gray-200  rounded-lg w-2/5"></div>
+      <div class="h-6 animate-shimmer bg-gray-200 w-3/5 rounded-lg"></div>
+      <div class="h-6 animate-shimmer bg-gray-200 w-3/4 rounded-lg"></div>
+
+    </div>
+
+  </div>
+
+</template>
+
+<style scoped>
+.animate-shimmer {
+  background: linear-gradient(90deg, #e5e7eb 25%, #f3f4f6 37%, #e5e7eb 63%);
+  background-size: 80% 400%;
+  animation: shimmer 1.4s ease infinite;
+}
+
+@keyframes shimmer {
+  0% { background-position: -400px 0; }
+  100% { background-position: 400px 0; }
+}
+</style>
